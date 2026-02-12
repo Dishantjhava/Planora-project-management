acb0e3a9990359be149575af9b20c3b83a65f62e
dishantjhava <dishantjava06690@gmail.com>
2026-02-13 01:50:04 +0530

    Title name updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -116,7 +116,7 @@ const Login = ({ onLogin }) => {
               <path d="M21 12V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H16" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
           </div>
-          <h1>Planora123</h1>
+          <h1>Planora</h1>
           <p>From planning to progress</p>
         </div>
 
