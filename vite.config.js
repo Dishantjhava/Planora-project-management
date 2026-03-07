4cffe746e25a9261845fb30af2720a648f01b8c4
dishantjhava <dishantjava06690@gmail.com>
2026-03-07 17:38:31 +0530

    fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Planora-project-management/",
   plugins: [react()],
   server: {
     proxy: {
